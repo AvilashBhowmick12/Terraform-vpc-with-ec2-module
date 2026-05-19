ad14e4db03199b5041a88ad16b3ff8668393c5e7
Avilash Bhowmick <avilash.12bhowmick@gmail.com>
2026-05-19 05:20:10 +0000

    chore: terraform fmt

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vpc" {
 }
 
 module "ec2" {
-  source     = "./modules/ec2"
-  vpc_id     = module.vpc.vpc_id
-  subnet_id  = module.vpc.public_subnet_ids[0]
+  source    = "./modules/ec2"
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.vpc.public_subnet_ids[0]
 }
